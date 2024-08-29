0c8fea3030df631290bb7578f42fb83231e691bf
brohudev <hitarth.thanki9006@gmail.com>
2024-08-29 13:39:49 -0500

    add pages, but it broken

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import Header from './components/header';
-import About from './components/about';
-import WhatIDo from './components/whatido';
-import {ProjectsSection} from './components/projects';
-import Contact from './components/contact';
-import Footer from './components/footer';
-import NoiseCanvas from './components/utils/noisecanvas';
+import { Route, Routes } from 'react-router-dom';
+
+// import Header from './components/header';
+// import About from './components/about';
+// import WhatIDo from './components/whatido';
+import {ProjectsSection} from './pages/projects';
+import Home from './pages/Home';
+// import Contact from './components/contact';
+// import Footer from './components/footer';
+// import NoiseCanvas from './components/utils/noisecanvas';
 
 const App = () => {
+    // return (
+    //     <div className="app">
+    //         <NoiseCanvas />
+    //         <Header />
+    //         <div className="content">
+    //             <About />
+    //             <WhatIDo />
+    //             <ProjectsSection />
+    //             <Contact />
+    //         </div>
+    //         <Footer />
+    //     </div>
+    // );
     return (
-        <div className="app">
-            <NoiseCanvas />
-            <Header />
-            <div className="content">
-                <About />
-                <WhatIDo />
-                <ProjectsSection />
-                <Contact />
-            </div>
-            <Footer />
+        <div className="App">
+          {/* <Navbar /> */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<ProjectsSection />} />
+            {/* <Route path="/experience" element={<Experience />} /> */}
+            {/* <Route path="/mis-stuff" element={<MisStuff />} /> */}
+          </Routes>
         </div>
-    );
+      );
+    
 };
 
 export default App;
